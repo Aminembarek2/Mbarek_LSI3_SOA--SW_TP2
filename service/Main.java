65b00029142235df7d79d20ffb78f5011de58bc2
Aminembarek2 <62722834+Aminembarek2@users.noreply.github.com>
2023-10-15 22:37:24 +0100

    first commit

---
+++ service/Main.java
@@ -0,0 +1,11 @@
+package service;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BanqueService b = new BanqueService();
+		System.out.println("Le montant totale est : "+b.conversion(13));
+
+	}
+
+}
